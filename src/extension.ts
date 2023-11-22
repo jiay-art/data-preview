bf42dce26e18be1643b07fda333c9c0609728ebb
jiay <934912625@qq.com>
2023-11-22 14:30:05 +0800

    解决第二次打开的时候数据未渲染问题

--- src/extension.ts
+++ src/extension.ts
@@ -3,47 +3,46 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
-// import { acquireVsCodeApi } from "vscode";
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  // console.log('Congratulations, your extension "data-preview" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  // let disposable = vscode.commands.registerCommand('data-preview.helloWorld', () => {
-  // 	// The code you place here will be executed every time your command is executed
-  // 	// Display a message box to the user
-  // 	vscode.window.showInformationMessage('Hello World from data-preview!');
-  // });
-
-  // context.subscriptions.push(disposable);
-
   vscode.window.registerWebviewViewProvider("myPluginView", {
     resolveWebviewView(webviewView) {
       // 从my-data.json中读取数据
       console.log("==========>resolveWebviewView");
+
       // 设置 localResourceRoots 允许加载外部资源
       webviewView.webview.options = {
         enableScripts: true,
         localResourceRoots: [vscode.Uri.file(context.extensionPath)],
       };
+
+      // 设置 Webview 的 HTML 内容
       webviewView.webview.html = getWebviewContent(context);
-      // 1. 获取数据
-      const configData = getConfigFileData();
-      //2. 传递数据
-      webviewView.webview.postMessage({
-        command: "updateConfig",
-        data: configData,
+
+      // 监听来自 Webview 的消息
+      webviewView.webview.onDidReceiveMessage((message) => {
+        console.log("收到来自 Webview 的消息", message);
+        if (message.command === "updateConfig") {
+          // 1. 获取数据,处理更新配置数据的逻辑
+          const updatedConfigData = getConfigFileData();
+          // 2. 传递数据,将数据发送给 Webview
+          webviewView.webview.postMessage({
+            command: "updateConfig",
+            data: updatedConfigData,
+          });
+        }
       });
     },
   });
 }
 
-//获取配置文件中的数据
+/**
+ * 获取配置文件中的数据
+ * 从当前目录的.vscode下的my-data.json中获取数据
+ * @returns
+ */
 function getConfigFileData(): any[] {
   try {
     const configPath = path.join(vscode.workspace.workspaceFolders![0].uri.fsPath, ".vscode", "my-data.json");
@@ -58,28 +57,16 @@ function getConfigFileData(): any[] {
   }
 }
 
-//渲染数据列表// 渲染数据列表
+/**
+ * 获取html页面数据
+ * @param context 
+ * @returns 
+ */
 function getWebviewContent(context: vscode.ExtensionContext): string {
   const onDiskPath = vscode.Uri.joinPath(context.extensionUri, "views", "list.html");
   // Set the HTML content of the webview
   const htmlContent = fs.readFileSync(onDiskPath.fsPath, "utf-8");
   return htmlContent;
-  // `
-  //   ${htmlContent}
-  //   <script>
-  //   // 等待 HTML 页面加载完成后再发送数据
-  //   // document.addEventListener('DOMContentLoaded', function() {
-  //   //   //updateConfigDisplay(${JSON.stringify(getConfigFileData())});
-  //   //   //console.log('hello world')
-  //   //   // 在扩展代码中发送消息
-  //   //   const vscode = acquireVsCodeApi();
-  //   //   vscode.postMessage({
-  //   //     command: 'updateConfig',
-  //   //     data: ${JSON.stringify(getConfigFileData())},
-  //   //   });
-  //   // });
-  //   </script>
-  // `;
 }
 
 // This method is called when your extension is deactivated
